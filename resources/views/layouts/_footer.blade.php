70df2fe8f78bbe4b430d57e4812fe88dadce6c4b
yangfan <yf244190857@gmail.com>
2020-11-18 20:59:24 +0800

    局部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+  <div class="container">
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+    <a href="https://learnku.com/laravel/courses" target=_blank>
+      刻意练习，每日精进
+    </a>
+
+    <div class="float-right">
+      <a href="/about" >关于</a>
+    </div>
+  </div>
+</footer>
