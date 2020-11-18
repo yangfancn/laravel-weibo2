09b2823be5fa6bc0677daadf17d25e229cba14c5
yangfan <yf244190857@gmail.com>
2020-11-18 21:05:32 +0800

    路由命名

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="#">Weibo App</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="#">帮助</a>
+        <a class="nav-link" href="{{ route('help') }}">帮助</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">登录</a>
