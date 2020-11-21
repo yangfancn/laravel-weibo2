817a6c3d0ec255798ff67b2aa17e3b6fffff3dc4
yangfan <244190857@qq.com>
2020-11-21 16:17:25 +0800

    login

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -8,12 +8,32 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('help') }}">帮助</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">登录</a>
-        </li>
+        @if (Auth::check())
+          <li class="nav-item">
+            <a href="#" class="nav-link">用户列表</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle user-photo" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img src="{{Auth::user()->gravatar()}}" alt="{{ Auth::user()->name }}">
+            </a>
+            <div class="dropdown-menu">
+              <a href="{{ route('users.show', [Auth::user()]) }}" class="dropdown-item">个人中心</a>
+              <a href="#" class="dropdown-item">编辑资料</a>
+              <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item">
+              <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                {{ method_field('DELETE') }}
+                <button class="btn btn-block btn-danger">退出</button>
+              </form>
+              </a>
+            </div>
+          </li>
+        @else
+          <li class="nav-item">
+            <a href="{{ route('login') }}" class="nav-link">登录</a>
+          </li>
+        @endif
       </ul>
     </div>
   </div>
