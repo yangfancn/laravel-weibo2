160c63aa7149a6b4439fdfa0c75b4e6b83847611
yangfan <244190857@qq.com>
2020-11-28 10:50:23 +0800

    用户微博列表

---
+++ resources/views/statuses/_status.blade.php
@@ -0,0 +1,9 @@
+<li class="media mt-4 mb-4">
+  <a href="{{ route('users.show', $user->id) }}" title="{{ $user->name }}">
+    <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar">
+  </a>
+  <div class="media-body">
+    <h5 class="mt-0 mb-1">{{ $user->name }}<small>{{ $status->created_at->diffForHumans() }}</small></h5>
+    <p class="mb-0">{{ $status->content }}</p>
+  </div>
+</li>
