e366b4d4af7d25a6165bfddfe8a681d1a0f97e46
yangfan <yf244190857@gmail.com>
2020-12-02 21:30:05 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,35 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderByDesc('created_at');
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function is_following($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
